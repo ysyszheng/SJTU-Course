460cfc8065777153452001df4cf929605c9cf644
Yusen Zheng <ysyszheng@gmail.com>
2022-12-27 12:25:46 +0800

    update

--- lab4/client.c
+++ lab4/client.c
@@ -1,32 +1,39 @@
 #include "sockcom.h"
 
 int main(int argc, char **argv) {
-  int sockfd;
+  int sockfd, n;
   struct sockaddr_in server;
   struct hostent *hp, *gethostbyname();
   char msg[1024];
   FILE *fsend = fopen("in.txt", "r");
+
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd < 0) {
-    perror("Opening stream socket");
+    printf("[ERROR]:\tsocket\n");
   }
+
   if ((hp = gethostbyname(argv[1])) == NULL) {
-    fprintf(stderr, "%s: Unknow\n", argv[1]);
+    printf("[ERROR]:\tUnknow hp %s\n", argv[1]);
     exit(2);
   }
+
   server.sin_family = AF_INET;
   bcopy((char *)hp->h_addr, (char *)&server.sin_addr.s_addr, hp->h_length);
   server.sin_port = htons(atoi(argv[2]));
+
   if (connect(sockfd, (struct sockaddr *)&server, sizeof(server)) < 0) {
-    perror("Connecting stream socket");
+    printf("[ERROR]:\tconnect\n");
   }
-  while ((fread(msg, 1, 1, fsend) > 0)) {
+
+  while ((n = fread(msg, 1, sizeof(msg), fsend)) > 0) {
     if (send(sockfd, msg, strlen(msg), 0) < 0) {
-      perror("Failed to send");
+      printf("[ERROR]:\tsend\n");
     }
+    printf("[INFO]:\tsend %d bytes", n);
     bzero(msg, sizeof(msg));
   }
-  printf("EOF\n");
+
+  printf("[INFO]:\tEOF\n");
   close(sockfd);
   fclose(fsend);
   exit(0);
--- lab4/server.c
+++ lab4/server.c
@@ -4,33 +4,43 @@ int main() {
   int newsockfd, sockfd, length, count;
   struct sockaddr_in server;
   char buf[1024];
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
   FILE *frecv;
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd < 0) {
-    perror("socket");
+    printf("[ERROR]:\tsocket\n");
   }
+
   server.sin_family = AF_INET;
   server.sin_addr.s_addr = INADDR_ANY;
   server.sin_port = 6666;
   server.sin_port = htons(server.sin_port);
+
   if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) < 0) {
-    perror("bind");
+    printf("[ERROR]:\tbind\n");
   }
+
   length = sizeof(server);
   if (getsockname(sockfd, (struct sockaddr *)&server, &length) < 0) {
-    perror("getsockname");
+    printf("[ERROR]:\tgetsockname\n");
   }
+
   listen(sockfd, 5);
+
   while (1) {
     newsockfd = accept(sockfd, (struct sockaddr *)0, (int *)0);
+    printf("[INFO]:\tconnect success\n");
+
     if (!fork()) {
       close(sockfd);
       bzero(buf, sizeof(buf));
       if ((count = recv(sockfd, buf, sizeof(buf), 0)) < 0) {
-        perror("read");
+        printf("[ERROR]:\tread\n");
       }
+
       frecv = fopen("out.txt", "w");
-      fwrite(buf, 1, 1, frecv);
+      fwrite(buf, 1, count, frecv);
+      printf("[INFO]:\twrite %d bytes\n", count);
       fclose(frecv);
     }
     close(newsockfd);
